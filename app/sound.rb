ed89513474cf3e052125085e4599697c8cd3eb94
Brett Chalupa <brettchalupa@gmail.com>
2023-04-07 07:52:46 -0400

    adjust: reduce global sfx vol
    got some feedback it was a bit too loud

--- app/sound.rb
+++ app/sound.rb
@@ -1,5 +1,8 @@
 def play_sfx(args, key)
   if args.state.setting.sfx
-    args.outputs.sounds << "sounds/#{key}.wav"
+    args.audio["sfx_#{key}"] = {
+      input: "sounds/#{key}.wav",
+      gain: 0.6,
+    }
   end
 end
